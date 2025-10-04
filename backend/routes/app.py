1799a4bbf2f2ff2929eefb4fe60fb75e32bb87de
Anthony Lu <alu34@student.ubc.ca>
2025-10-04 13:55:18 -0700

    Add API endpoint for image processing

--- backend/routes/app.py
+++ backend/routes/app.py
@@ -1,6 +1,8 @@
 from typing import Union
 
 import uvicorn
+import numpy as np
+import base64
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -13,6 +15,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/detection/process")
+def process(base64_image: str):
+    # call models/libraries on base64_image to analyze the image
+    in_danger = True
+    return {"danger": in_danger}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000);
