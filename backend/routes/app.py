89f1f2581f3e07a306756c885a91e59d5d66e7df
Anthony Lu <alu34@student.ubc.ca>
2025-10-04 16:40:04 -0700

    Add fire functionality into API

--- backend/routes/app.py
+++ backend/routes/app.py
@@ -1,9 +1,12 @@
 from typing import Union
 
+import cv2
 import uvicorn
 import numpy as np
 import base64
 from fastapi import FastAPI
+from backend.services.core import process_image
+from services.models.fire_detector import detect_fire_and_smoke
 
 app = FastAPI()
 
@@ -18,9 +21,10 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.post("/detection/process")
 def process(base64_image: str):
-    # call models/libraries on base64_image to analyze the image
+    detections = process_image(base64_image)
     in_danger = True
-    return {"danger": in_danger}
+    return {"detections": detections, "danger": in_danger}
 
+    return {"detections": detections}
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000);
