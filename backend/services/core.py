c179ec1b85704a3e1a34283d6f71b78c3eeb8499
ryanraen <ryan.xie129@gmail.com>
2025-10-04 14:34:40 -0700

    added ambiguous incident case in core

--- backend/services/core.py
+++ backend/services/core.py
@@ -1,5 +1,6 @@
 from google import genai
 from google.genai import types
+import base64
 
 GEMINI_MODEL = 'gemini-2.5-flash'
 try:
@@ -7,12 +8,21 @@ try:
 except Exception as e:
     print(e)
 
-def process_image(image_bytes: base64):
+def process_image(frame: base64, context: dict) -> list[str]:
+    """
+    Main app orchestration pipeline
+    frame: base64 image of a captured frame from camera feed
+    context: dict of e.g., {'room': 'kitchen', 'timestamp': ...}
+    """
+    
+    response = []
+    
+    # ambiguous incident case
     response = client.models.generate_content(
     model='gemini-2.5-flash',
     contents=[
         types.Part.from_bytes(
-        data=image_bytes,
+        data=frame,
         mime_type='image/jpeg',
         ),
         "placeholder LM call",
